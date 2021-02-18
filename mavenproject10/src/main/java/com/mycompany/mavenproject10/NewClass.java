6f796886c422cd3558c07d292e6a670033fed536
Adriel Lopez <alopez216@hawkmail.hccfl.edu>
2021-02-18 18:28:20 -0500

    new line made

---
+++ mavenproject10/src/main/java/com/mycompany/mavenproject10/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject10;
+
+/**
+ *
+ * @author Adriel
+ */
+public class NewClass {
+        public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+    
+}
